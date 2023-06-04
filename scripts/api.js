6ef55a48a3e246a0e282d1311fda178c9860edb1
hamart1a <n0netheless@naver.com>
2023-06-04 23:23:58 +0900

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit 226b99badff7482eab16923da60464bdf184b646.

--- scripts/api.js
+++ scripts/api.js
@@ -51,30 +51,16 @@ class API {
 }
 
 class PATH {
-    constructor(path, parameters={}) {
-        this.path = path;
-        this.parameters = parameters;
+    constructor(string) {
+        this.value = string;
     }
 
-    get REVIEWS() { return new PATH(this.path + "/reviews", this.parameters) }
-    get TAGS() { return new PATH(this.path + "/tags", this.parameters) }
+    get REVIEWS() { return new PATH(this.value + "/reviews") }
+    get TAGS() { return new PATH(this.value + "/tags") }
 
-    PARAM = (key, value) => { 
-        this.parameters[key] = value;
-        return new PATH(this.path, this.parameters);
-    }
+    get url() { return this.value + '/'; }
 
-
-    get url() {
-        let url = this.path + '/?';
-        console.log(this.parameters);
-        for (const key in this.parameters) {
-            url += `${key}=${this.parameters[key]}`;
-        }
-        return url; 
-    }
-
-    of = (string) => { return new PATH(this.path + '/' + string, this.parameters) }
+    of = (string) => { return new PATH(this.value + '/' + string) }
 }
 
 const api = new API;
--- scripts/modal.js
+++ scripts/modal.js
@@ -12,21 +12,9 @@ function showModal(place) {
     document.querySelector('.space_id').innerHTML = place.id;
 }
 
-function clearTags() {
-  keyElement.innerHTML = "";
-}
-
-function showModal() {
-  modal.style.display = "block";
-  clearTags();
-  let contentInput = document.getElementById("content");
-  contentInput.value = "";
-  document.getElementById("khuong_3").checked = true;
-}
-
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+closeBtn.addEventListener('click', ()=> {
+    modal.style.display = "none";
+  });
 
 
 
